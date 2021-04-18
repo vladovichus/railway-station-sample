5b4fd01d9f962f226aa557b0980343cd817f45b7
Vladislav Milovanov <vladovichus@gmail.com>
2021-04-18 20:35:50 +0300

    Add lists of all and active characters

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -7,21 +8,40 @@ namespace RailwayStationSample
 {
     public class Character : MonoBehaviour
     {
+        public static List<Character> ActiveCharacters = new List<Character>();
+        public static List<Character> AllCharacters = new List<Character>();
+        
         [Header("Settings")]
         public float MinChangeTargetWaitingDuration = 1f;
         public float MaxChangeTargetWaitingDuration = 5f;
         
         [Header("Components")]
         public NavMeshAgent Agent;
+        public Transform[] DestroyAreas;
         
         [Header("Target")]
         public Anchor TargetAnchor;
 
         private float _cooldownTime;
+        private bool _isActive;
 
         private void Reset()
         {
             Agent = GetComponentInChildren<NavMeshAgent>();
+        }
+
+        private void Awake()
+        {
+            _isActive = true;
+            
+            ActiveCharacters.Add(this);
+            AllCharacters.Add(this);
+        }
+
+        private void OnDestroy()
+        {
+            ActiveCharacters.Remove(this);
+            AllCharacters.Remove(this);
         }
 
         private void Start()
@@ -31,6 +51,11 @@ namespace RailwayStationSample
 
         private void Update()
         {
+            if (!_isActive)
+            {
+                return;
+            }
+            
             if (IsFinished())
             {
                 _cooldownTime -= Time.deltaTime;
@@ -53,6 +78,17 @@ namespace RailwayStationSample
             TargetAnchor = targetAnchor;
 
             Agent.SetDestination(TargetAnchor.transform.position);
+        }
+
+        [ContextMenu("Send to Destroy Area")]
+        public void SendToDestroyArea()
+        {
+            _isActive = false;
+            ActiveCharacters.Remove(this);
+
+            Transform randomDestroyArea = DestroyAreas[Random.Range(0, DestroyAreas.Length - 1)];
+
+            Agent.SetDestination(randomDestroyArea.position);
         }
 
         private bool IsFinished() => !Agent.hasPath || Agent.hasPath && Agent.remainingDistance <= Agent.stoppingDistance + 1f;
