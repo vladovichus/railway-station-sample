4392e61ec54691cac576ce90a272e2d710f63895
Vladislav Milovanov <vladovichus@gmail.com>
2021-04-18 18:52:39 +0300

    Add CharacterSpawner script

---
+++ Assets/Scripts/CharacterSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RailwayStationSample
+{
+    public class CharacterSpawner : MonoBehaviour
+    {
+        public GameObject CharacterPrefab;
+        
+        public int MinSpawnCount = 1;
+        public int MaxSpawnCount = 5;
+
+        private int _nameIndexCounter;
+        
+        [ContextMenu("Spawn")]
+        public void Spawn()
+        {
+            int spawnCount = Random.Range(MinSpawnCount, MaxSpawnCount);
+
+            for (int i = 0; i < spawnCount; i++)
+            {
+                SpawnCharacter();   
+            }
+        }
+
+        private void SpawnCharacter()
+        {
+            GameObject go = Instantiate(CharacterPrefab, transform.position, Quaternion.identity);
+
+            go.name = $"Character (Bus #{_nameIndexCounter})";
+            _nameIndexCounter++;
+        }
+    }
+}
